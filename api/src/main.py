a86db62283a2e12c6cd1f255868dac5e42126da3
Ichiro Kobayashi <kobayashi@supinf.co.jp>
2021-05-12 14:22:26 +0900

    put api path in the right file

--- api/src/main.py
+++ api/src/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask, jsonify
 from flask_cors import CORS
-import db
 import router
 
 app = Flask(__name__)
@@ -14,31 +13,6 @@ CORS(
         r"/api/*": {"origins": ["http://localhost", "http://localhost:4200"]}
     }
 )
-
-@app.route("/", methods=['GET'])
-def main():
-    return "Hello World!!"
-
-@app.route("/create/<name>", methods=['GET'])
-def create(name):
-    db = db.getConnection()
-    cur = db.cursor()
-    sql = "select * from members"
-    cur.execute(sql)
-    members = cur.fetchall()
-    cur.close()
-    db.close()
-    return jsonify({
-            'status':'OK',
-            'members':members
-        })
-
-@app.after_request
-def after_request(response):
-  # response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8080, threaded=True)
--- api/src/router.py
+++ api/src/router.py
@@ -3,6 +3,17 @@ from controller import scrape_controller
 
 router = Blueprint('router', __name__)
 
+@router.route("/", methods=['GET'])
+def hello_world():
+    return "Hello World!!"
+
 @router.route("/api/v1/scrape", methods=['GET'])
 def api_v1_scrape():
     return scrape_controller.scrape()
+
+@router.after_request
+def after_request(response):
+  # response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
