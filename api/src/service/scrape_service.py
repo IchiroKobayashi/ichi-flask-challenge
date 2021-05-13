23fa1fedd08c2269270be4210663b6b8ff17125f
Ichiro Kobayashi <kobayashi@supinf.co.jp>
2021-05-13 17:44:38 +0900

    optimize memory usage by closing file object properly

--- api/src/service/scrape_service.py
+++ api/src/service/scrape_service.py
@@ -18,40 +18,41 @@ def scrape_logic():
     return jsonify(result)
 
 def edit_text():
-    text = open("./text/keisuke_honda.txt","r").read()
-    table = str.maketrans({
-            # '\n': '',
-            # '\r': '',
-            '(': '（',
-            ')': '）',
-            '[': '［',
-            ']': '］',
-            '"':'”',
-            "'":"’",
-        })
-    text = text.translate(table).split()
+    with open("./text/keisuke_honda.txt","r") as f:
+        text = f.read()
+        table = str.maketrans({
+                # '\n': '',
+                # '\r': '',
+                '(': '（',
+                ')': '）',
+                '[': '［',
+                ']': '］',
+                '"':'”',
+                "'":"’",
+            })
+        texts = text.translate(table).split()
     url_pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
 
     for _ in range(10):# TODO: 調査=>なんだか10回くらいやらないとちゃんと文字列が精製されない。
-        for line in text:
+        for line in texts:
         # for word in line:
             if re.match(url_pattern, line): # URL
-                text.remove(line)
+                texts.remove(line)
             elif bool(re.search(r'[a-zA-Z0-9]', line)):
-                text.remove(line)
+                texts.remove(line)
             elif re.match('^@.*', line):
-                text.remove(line)
+                texts.remove(line)
             elif re.match('.*,$', line):
-                text.remove(line)
+                texts.remove(line)
             elif re.match('^#.*', line):
-                text.remove(line)
+                texts.remove(line)
             elif re.match('RT', line):
-                text.remove(line)
+                texts.remove(line)
 
     # Parse text using MeCab
     m = MeCab.Tagger('-Owakati')
     f = open('./text/new_keisuke_honda.txt', 'w')
-    for line in text:
+    for line in texts:
         splited_line = m.parse(line)
         f.write(str(splited_line))
     f.close()
@@ -59,7 +60,8 @@ def edit_text():
 
 def make_dictionary():
     # Load file
-    text = open("./text/new_keisuke_honda.txt", "r").read()
+    with open("./text/new_keisuke_honda.txt", "r") as f:
+        text = f.read()
 
     # Build model
     text_model = markovify.NewlineText(text, state_size=3, well_formed=False)
