e98acf554d2388ccd42eaaa926f0731c5614e055
Ichiro Kobayashi <kobayashi@supinf.co.jp>
2021-05-21 22:55:05 +0900

    generate text data base on a new filtering logic

--- api/src/service/text_editor.py
+++ api/src/service/text_editor.py
@@ -15,27 +15,28 @@ table = str.maketrans({
 text = text.translate(table).split()
 url_pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
 
-print(text)
-for i in range(10):# TODO: 調査=>なんだか10回くらいやらないとちゃんと文字列が精製されない。
-    for line in text:
-    # for word in line:
-        if re.match(url_pattern, line): # URL
-            text.remove(line)
-        elif bool(re.search(r'[a-zA-Z0-9]', line)):
-            text.remove(line)
-        elif re.match('^@.*', line):
-            text.remove(line)
-        elif re.match('.*,$', line):
-            text.remove(line)
-        elif re.match('^#.*', line):
-            text.remove(line)
-        elif re.match('RT', line):
-            text.remove(line)
+text = list(set(text)) # Remove duplicates
+
+for i in range(10):
+    for phrase in text:
+    # for word in phrase:
+        if re.match(url_pattern, phrase): # URL
+            text.remove(phrase)
+        elif bool(re.search(r'[a-zA-Z0-9]', phrase)):
+            text.remove(phrase)
+        elif re.match('^@.*', phrase):
+            text.remove(phrase)
+        elif re.match('.*,$', phrase):
+            text.remove(phrase)
+        elif re.match('^#.*', phrase):
+            text.remove(phrase)
+        elif re.match('RT', phrase):
+            text.remove(phrase)
 
 # Parse text using MeCab
 m = MeCab.Tagger('-Owakati')
 f = open('../text/new_keisuke_honda.txt', 'w')
-for line in text:
-    splited_line = m.parse(line)
-    f.write(str(splited_line))
+for phrase in text:
+    splited_phrase = m.parse(phrase)
+    f.write(str(splited_phrase))
 f.close()
